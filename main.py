0c39ddc11037bb1785f504507d568037e1d112b0
Shubhadeepnath <shubhadpn1927@gmail.com>
2025-05-08 02:01:09 +0530

    added weather script using dotenv

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import speech_recognition as sr
 import webbrowser
 import pyttsx3
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
@@ -12,24 +17,50 @@ def speak(text):
 
 
 
-def get_weather(city):
-    api_key = "db2bf7219f9f2567e314a6c2ad52275d"  # replace with your actual API key
-    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+# def get_weather(city):
+#     api_key ="50a0b7fedbc6ed1783655588d3de94a0"  # replace with your actual API key
 
+    
+#     base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+#     complete_url = f"{base_url}appid={api_key}&q={city}&units=metric"
+
+#     response = requests.get(complete_url)
+#     data = response.json()
+
+#     if data["cod"] != "404":
+#         main = data["main"]
+#         weather_desc = data["weather"][0]["description"]
+#         temp = main["temp"]
+#         humidity = main["humidity"]
+#         result = f"Currently in {city}, it's {temp}°C with {weather_desc} and {humidity}% humidity."
+#         return result 
+#     else:
+#         return "City not found. Please try again."
+
+def get_weather(city):
+    api_key = os.getenv("OPENWEATHER_API_KEY")
+    # print(f"API Key: {api_key}") 
+    if not api_key:
+        print("API key not found. Make sure it's in the .env file.")
+        return
+
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
     complete_url = f"{base_url}appid={api_key}&q={city}&units=metric"
 
     response = requests.get(complete_url)
     data = response.json()
+    # print(f"API response: {data}")
 
     if data["cod"] != "404":
-        main = data["main"]
-        weather_desc = data["weather"][0]["description"]
-        temp = main["temp"]
-        humidity = main["humidity"]
-        result = f"Currently in {city}, it's {temp}°C with {weather_desc} and {humidity}% humidity."
-        return result 
+         main = data["main"]
+         weather_desc = data["weather"][0]["description"]
+         temp = main["temp"]
+         humidity = main["humidity"]
+         result = f"Currently in {city}, it's {temp}°C with {weather_desc} and {humidity}% humidity."
+         return result 
     else:
-        return "City not found. Please try again."
+         return "City not found. Please try again."
 
 
 def processCommand(c):
@@ -52,7 +83,9 @@ def processCommand(c):
             with sr.Microphone() as source:
                 audio = recognizer.listen(source, timeout=4, phrase_time_limit=3)
                 city = recognizer.recognize_google(audio)
+                # print(f"You said: {city}") 
                 weather = get_weather(city)
+               
                 speak(weather)
         except:
             speak("Sorry, I couldn't catch the city name.")
